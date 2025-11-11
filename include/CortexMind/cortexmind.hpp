cdf923837cb78179e508cdfb94150213487e0b1a
Muhammet Kaya <muham123cak@gmail.com>
2025-11-11 09:50:07 +0300

    change:file path

---
+++ include/CortexMind/cortexmind.hpp
@@ -0,0 +1,22 @@
+//
+// Created by muham on 8.11.2025.
+//
+
+#ifndef CORTEXMIND_CORTEXMIND_HPP
+#define CORTEXMIND_CORTEXMIND_HPP
+
+#include <Mind/Model/model.hpp>
+#include <Mind/Model/sequential.hpp>
+#include <Mind/NeuralNetwork/conv.hpp>
+#include <Mind/NeuralNetwork/dense.hpp>
+#include <Mind/NeuralNetwork/batch_norm.hpp>
+
+#include <Utils/log.hpp>
+#include <Utils/params.hpp>
+#include <Utils/MathTools/random.hpp>
+
+#include <DataPrep/data.hpp>
+#include <DataPrep/image_data.hpp>
+#include <DataPrep/tokenizer.hpp>
+
+#endif //CORTEXMIND_CORTEXMIND_HPP
--- test/main.cpp
+++ test/main.cpp
@@ -1,3 +1,4 @@
 //
 // Created by muham on 8.11.2025.
 //
+
