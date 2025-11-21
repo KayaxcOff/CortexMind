2a3e27eb9dcb8a699e90bb80da6f75f4e129a680
Muhammet Kaya <muham123cak@gmail.com>
2025-11-21 20:32:18 +0300

    add : random weights

--- include/CortexMind/Utils/MathTools/random.hpp
+++ include/CortexMind/Utils/MathTools/random.hpp
@@ -27,4 +27,24 @@ inline std::vector<std::vector<cortex::float32>> random_seed() {
     return seed;
 }
 
+inline std::vector<cortex::tensor> random_weights(const cortex::size in, const cortex::size out) {
+    std::vector<cortex::tensor> weights;
+
+    std::random_device rd;
+    std::mt19937 gen(rd());
+    std::uniform_real_distribution dis(-1.0f, 1.0f);
+
+    cortex::tensor weight_row{{}, {}};
+
+    for (cortex::size i = 0; i < in; ++i) {
+
+        for (cortex::size j = 0; j < out; ++j) {
+            weight_row = cortex::tensor{{static_cast<size_t>(dis(gen))}, {}};
+        }
+        weights.push_back(weight_row);
+    }
+
+    return weights;
+}
+
 #endif //CORTEXMIND_RANDOM_HPP
