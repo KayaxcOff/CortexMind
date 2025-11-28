216b5ddf497814b5f7a526a13bfe8df812636133
Muhammet Kaya <muham123cak@gmail.com>
2025-11-28 15:07:23 +0300

    complete : model class

--- include/CortexMind/Mind/Model/model.hpp
+++ include/CortexMind/Mind/Model/model.hpp
@@ -19,25 +19,78 @@ namespace cortex::model {
     public:
         template<typename  T, typename... Args>
         void add(Args... args) {
-            layers.emplace_back(std::make_unique<T>(args...));
+            this->layers.emplace_back(std::make_unique<T>(args...));
         }
 
         template<typename L, typename O, typename A>
         void compile(float64 lr) {
-            loss_fn_ = std::make_unique<L>();
-            optim_fn_ = std::make_unique<O>(lr);
-            activation_fn_ = std::make_unique<A>();
+            this->loss_fn_ = std::make_unique<L>();
+            this->optim_fn_ = std::make_unique<O>(lr);
+            this->activation_fn_ = std::make_unique<A>();
         }
 
-        void fit();
+        void fit(const std::vector<tensor>& X, const std::vector<tensor>& Y, const size epochNum = 1, const size batchSize = 32) {
+            const size numSamples = X.size();
+
+            if (X.size() != Y.size()) {
+                throw std::invalid_argument("Input and output data size must be the same.");
+            }
+
+            for (size i = 0; i <epochNum; ++i) {
+                float64 epochLoss = 0.0;
+
+                for (size start=0; start < numSamples; ++start) {
+                    const size end = std::min(start + batchSize, numSamples);
+
+                    std::vector<tensor> batch_outputs;
+                    for (size j = start; j < end; ++j) {
+                        tensor output = X[j];
+                        for (const auto &layer : this->layers) {
+                            output = layer->forward(output);
+                        }
+                        batch_outputs.push_back(output);
+                    }
+
+                    std::vector<tensor> batch_grads;
+                    for (size j = 0; j < batch_outputs.size(); ++j) {
+                        epochLoss += this->loss_fn_->forward(batch_outputs[j], Y[start + j]);
+                        batch_grads.push_back(this->loss_fn_->backward(batch_outputs[j], Y[start + j]));
+                    }
+
+                    for (size j = 0; j < batch_grads.size(); ++j) {
+                        tensor grad = batch_grads[j];
+                        for (auto it = this->layers.rbegin(); it != this->layers.rend(); ++it) {
+                            grad = (*it)->backward(grad);
+                        }
+                    }
+
+                    for (const auto& it : this->layers) {
+                        tensor& params = it->getParams();
+                        tensor& grads = it->getGrads();
+
+                        this->optim_fn_->step(params, grads);
+                    }
+                }
+                std::cout << "Epoch " << i + 1
+                  << " / " << epochNum
+                  << " - Loss: " << epochLoss / numSamples
+                  << std::endl;
+            }
+        }
 
         void summary() const {
-            for (auto &layer : layers) {
+            for (auto &layer : this->layers) {
                 std::cout << layer->get_config() << std::endl;
             }
         }
 
-        tensor predict(const tensor &input);
+        tensor predict(const tensor &input) const {
+            tensor output = input;
+            for (const auto &layer : this->layers) {
+                output = layer->forward(output);
+            }
+            return output;
+        }
     private:
         std::vector<std::unique_ptr<nn::Layer>> layers;
         std::unique_ptr<loss::Loss> loss_fn_;
