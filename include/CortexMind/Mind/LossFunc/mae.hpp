d05c57bde3699737bd8896265ba6245eb4ecf993
Muhammet Kaya <muham123cak@gmail.com>
2025-11-28 10:30:42 +0300

    complete: loss functions classes but there is need an operator

---
+++ include/CortexMind/Mind/LossFunc/mae.hpp
@@ -0,0 +1,21 @@
+//
+// Created by muham on 28.11.2025.
+//
+
+#ifndef CORTEXMIND_MAE_HPP
+#define CORTEXMIND_MAE_HPP
+
+#include "loss.hpp"
+
+namespace cortex::loss {
+    class MeanAbsolute final : public Loss {
+    public:
+        MeanAbsolute();
+        ~MeanAbsolute() override;
+
+        tensor forward(const tensor &y_true, const tensor &y_pred) override;
+        tensor backward(const tensor &y_true, const tensor &y_pred) override;
+    };
+}
+
+#endif //CORTEXMIND_MAE_HPP
--- src/CortexMind/Mind/Loss/mae.cpp
+++ src/CortexMind/Mind/Loss/mae.cpp
@@ -1,3 +1,52 @@
 //
 // Created by muham on 9.11.2025.
 //
+
+#include "CortexMind/Mind/LossFunc/mae.hpp"
+
+using namespace cortex::loss;
+
+MeanAbsolute::MeanAbsolute() = default;
+
+MeanAbsolute::~MeanAbsolute() = default;
+
+cortex::tensor MeanAbsolute::forward(const tensor &y_true, const tensor &y_pred) {
+    const size cols = y_true.get_cols();
+    const size rows = y_true.get_rows();
+
+    if (cols != y_pred.get_cols() || rows != y_pred.get_rows()) {
+        throw std::invalid_argument("MeanAbsolute Loss: y_true and y_pred must have the same shape.");
+    }
+
+    float64 sum_abs = 0;
+
+    for (size i = 0; i < rows; ++i) {
+        for (size j = 0; j < cols; ++j) {
+            sum_abs += std::abs(y_true(i, j) - y_pred(i, j));
+        }
+    }
+
+    const float64 avg = sum_abs / static_cast<float64>(rows * cols);
+
+    tensor out(1, 1);
+    out(0, 0) = avg;
+    return out;
+}
+
+cortex::tensor MeanAbsolute::backward(const tensor &y_true, const tensor &y_pred) {
+    const size cols = y_true.get_cols();
+    const size rows = y_true.get_rows();
+
+    tensor gradient(rows, cols);
+    const auto n = static_cast<float64>(rows * cols);
+
+    for (size i = 0; i < rows; ++i) {
+        for (size j = 0; j < cols; ++j) {
+            if (const float64 diff = y_true(i, j) - y_pred(i, j); diff > 0) gradient(i, j) / n;
+            else if (diff < 0) gradient(i, j) / -n;
+            else gradient(i, j) = 0;
+        }
+    }
+
+    return gradient;
+}
