14b244928e56df7582ac14f2340ef429e2fde3a6
Muhammet Kaya <muham123cak@gmail.com>
2025-12-21 22:19:19 +0300

    test : testing scale on a tensor vector

--- test/test.cpp
+++ test/test.cpp
@@ -3,38 +3,35 @@
 //
 
 #include <CortexMind/cortexmind.hpp>
+#include <vector>
 
 using namespace cortex;
 
 int main() {
+    std::vector<tensor> array;
 
-    tensor input(2, 2, 2, 2);
-    input.fill(240);
+    for (int i = 0; i < 10; i++) {
+        tensor x(1, 1, 2, 2);
+        x.uniform_rand();
+        array.push_back(x);
+    }
 
-    const tensor output = ds::TensorScale::scale(input);
-    output.print();
+    for (int i = 0; i < array.size(); i++) {
+        array[i] = ds::TensorScale::scale(array[i]);
+    }
+
+    log("3. Index of Array: ");
+    array[3].print();
 
     return 0;
 }
 
 /*
---- output ---
-Tensor shape: [2, 2, 2, 2]
+---- output ----
+[LOG]: 3. Index of Array:
+Tensor shape: [1, 1, 2, 2]
 Batch 0:
  Channel 0:
- [ 0.941176 0.941176 ]
- [ 0.941176 0.941176 ]
-
- Channel 1:
- [ 0.941176 0.941176 ]
- [ 0.941176 0.941176 ]
-
-Batch 1:
- Channel 0:
- [ 0.941176 0.941176 ]
- [ 0.941176 0.941176 ]
-
- Channel 1:
- [ 0.941176 0.941176 ]
- [ 0.941176 0.941176 ]
- */
+ [ 0.00171939 0.00175495 ]
+ [ 0.000173517 2.90936e-05 ]
+*/
