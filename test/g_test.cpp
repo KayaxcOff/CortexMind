f5b858aa5144029393946a303c31b91e91f82a74
Muhammet Kaya <muham123cak@gmail.com>
2026-04-15 07:43:32 +0300

    feat: use GTest

--- test/g_test.cpp
+++ test/g_test.cpp
@@ -4,3 +4,24 @@
 
 #include <CortexMind/cortexmind.hpp>
 #include <gtest/gtest.h> // from _deps/googletest-src
+
+TEST(CXM_CUDA_AVAILABLE_TEST, CompileTimeGateWorks) {
+    #if !CXM_IS_CUDA_AVAILABLE
+        EXPECT_FALSE(cortex::has_cuda());
+    #else //#if !CXM_IS_CUDA_AVAILABLE
+        SUCCEED();
+    #endif //#if !CXM_IS_CUDA_AVAILABLE #else
+}
+
+TEST(CudaAvailabilityTest, IsDeterministic) {
+    const bool first = cortex::has_cuda();
+    const bool second = cortex::has_cuda();
+
+    EXPECT_EQ(first, second);
+}
+
+TEST(CudaAvailabilityTest, TrueImpliesCompileFlag) {
+    if (cortex::has_cuda()) {
+        EXPECT_TRUE(CXM_IS_CUDA_AVAILABLE);
+    }
+}
