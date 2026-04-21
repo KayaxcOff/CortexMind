356cf7ade262441b18260786b21becb8f699cf62
Muhammet Kaya <muham123cak@gmail.com>
2026-04-21 15:51:11 +0300

    feat: restart to grad

--- source/CortexMind/framework/Gradient/operations.cpp
+++ source/CortexMind/framework/Gradient/operations.cpp
@@ -3,3 +3,35 @@
 //
 
 #include "CortexMind/framework/Gradient/operations.hpp"
+#include <CortexMind/framework/Tensor/tensor.hpp>
+
+using namespace cortex::_fw::meta;
+using namespace cortex::_fw;
+
+addition::addition(const std::shared_ptr<TensorStorage> &tx_s, const std::shared_ptr<TensorStorage> &ty_s) : GradientFlow(1) {
+    this->tx_s = tx_s;
+
+    this->tx_s->shape = tx_s->shape;
+    this->tx_s->stride = tx_s->stride;
+    this->tx_s->offset = tx_s->offset;
+
+    this->ty_s = ty_s;
+
+    this->ty_s->shape = ty_s->shape;
+    this->ty_s->stride = ty_s->stride;
+    this->ty_s->offset = ty_s->offset;
+}
+
+void addition::backward(MindTensor *_grad) {
+    MindTensor tx(this->tx_s);
+    MindTensor ty(this->ty_s);
+
+    if (tx.requires_grad()) {
+        tx.grad() += *_grad;
+        tx.backward(*_grad);
+    }
+    if (ty.requires_grad()) {
+        ty.grad() += *_grad;
+        ty.backward(*_grad);
+    }
+}
--- source/CortexMind/framework/Gradient/operations.hpp
+++ source/CortexMind/framework/Gradient/operations.hpp
@@ -5,8 +5,22 @@
 #ifndef CORTEXMIND_FRAMEWORK_GRADIENT_OPERATIONS_HPP
 #define CORTEXMIND_FRAMEWORK_GRADIENT_OPERATIONS_HPP
 
+#include <CortexMind/framework/Gradient/flow.hpp>
+#include <memory>
+
 namespace cortex::_fw {
     struct TensorStorage;
 } //namespace cortex::_fw
+
+namespace cortex::_fw::meta {
+    struct addition : GradientFlow {
+        addition(const std::shared_ptr<TensorStorage>& tx_s, const std::shared_ptr<TensorStorage>& ty_s);
+
+        void backward(MindTensor *_grad) override;
+    private:
+        std::shared_ptr<TensorStorage> tx_s;
+        std::shared_ptr<TensorStorage> ty_s;
+    };
+} //namespace cortex::_fw::meta
 
 #endif //CORTEXMIND_FRAMEWORK_GRADIENT_OPERATIONS_HPP
