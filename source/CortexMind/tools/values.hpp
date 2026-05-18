558ed57f28ccda0ac137a218c4c8700741d3a6ea
Muhammet Kaya <muham123cak@gmail.com>
2026-05-18 20:11:25 +0300

    feat: new default values

--- source/CortexMind/tools/values.hpp
+++ source/CortexMind/tools/values.hpp
@@ -6,10 +6,15 @@
 #define CORTEXMIND_TOOLS_VALUES_HPP
 
 #include <CortexMind/framework/Memory/device_type.hpp>
+#include <CortexMind/runtime/macros.hpp>
+#include <CortexMind/tools/types.hpp>
 
 namespace cortex {
     inline auto host = _fw::sys::DeviceType::kHOST;
     inline auto cuda = _fw::sys::DeviceType::kCUDA;
+
+    inline int32 exit       = CXM_EXIT;
+    inline int32 err_exit   = CXM_ERR_EXIT;
 } //namespace cortex
 
 #endif //CORTEXMIND_TOOLS_VALUES_HPP
