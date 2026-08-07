afcb41020b191e0b0007a35cbbcc5f3833aa716b
Muhammet Kaya <muham123cak@gmail.com>
2026-08-07 21:16:34 +0300

    feat: test for TensorType

--- tests/main.cpp
+++ tests/main.cpp
@@ -2,11 +2,15 @@
 // Created by muham on 3.08.2026.
 //
 
-#include "CortexMind/framework/Tools/errors.hpp"
+#include "CortexMind/framework/Type/type.hpp"
+#include <iostream>
 
 using namespace cortex::_fw;
 
 int main() {
-    CXM_WARN(true, "This is a test");
+    const TensorType type(DType::BFloat16);
+
+    std::cout << type.ToString() << std::endl;
+
     return 0;
 }
