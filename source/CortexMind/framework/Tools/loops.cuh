0f0354f513044a08ec39d697db442c91c81f048e
Muhammet Kaya <muham123cak@gmail.com>
2026-08-07 15:58:06 +0300

    feat: added kernel loops for gpu kernels

---
+++ source/CortexMind/framework/Tools/loops.cuh
@@ -0,0 +1,20 @@
+//
+// Created by muham on 7.08.2026.
+//
+
+#ifndef CORTEXMIND_FRAMEWORK_TOOLS_LOOPS_CUH
+#define CORTEXMIND_FRAMEWORK_TOOLS_LOOPS_CUH
+
+#include <device_launch_parameters.h>
+
+#define CXM_KERNEL_LOOP_1D(i, N)                                \
+    for(std::size_t i = blockIdx.x * blockDim.x + threadIdx.x;  \
+    i < (N);                                                    \
+    i += blockDim.x * gridDim.x)
+
+#define CXM_KERNEL_LOOP_TAIL(i, tail_start, N)                          \
+    for (size_t i = tail_start + blockIdx.x * blockDim.x + threadIdx.x; \
+    i < (N);                                                            \
+    i += blockDim.x * gridDim.x)
+
+#endif //CORTEXMIND_FRAMEWORK_TOOLS_LOOPS_CUH
