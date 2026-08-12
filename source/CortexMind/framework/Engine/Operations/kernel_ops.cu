2913e3ddefc59d1adf54d21c6e18ba29a7d23667
Muhammet Kaya <muham123cak@gmail.com>
2026-08-12 11:17:17 +0300

    feat: working to gpu kernels

--- source/CortexMind/framework/Engine/Operations/kernel_ops.cu
+++ source/CortexMind/framework/Engine/Operations/kernel_ops.cu
@@ -6,14 +6,116 @@
 
 using namespace cortex::_fw::ops;
 
-float Addition::operator()(const float Xx, const float Xy) const noexcept {
+CXM_DEVICE_ATTR float Addition::operator()(const float Xx, const float Xy) const noexcept {
     return Xx + Xy;
 }
 
-tlx::bfloat16 Addition::operator()(const tlx::bfloat16 Xx, const tlx::bfloat16 Xy) const noexcept {
+CXM_DEVICE_ATTR tlx::bfloat16 Addition::operator()(const tlx::bfloat16 Xx, const tlx::bfloat16 Xy) const noexcept {
     return Xx + Xy;
 }
 
-tlx::half Addition::operator()(const tlx::half Xx, const tlx::half Xy) const noexcept {
+CXM_DEVICE_ATTR tlx::half Addition::operator()(const tlx::half Xx, const tlx::half Xy) const noexcept {
     return Xx + Xy;
 }
+
+CXM_DEVICE_ATTR float Subtraction::operator()(const float Xx, const float Xy) const noexcept {
+    return Xx - Xy;
+}
+
+CXM_DEVICE_ATTR tlx::bfloat16 Subtraction::operator()(const tlx::bfloat16 Xx, const tlx::bfloat16 Xy) const noexcept {
+    return Xx - Xy;
+}
+
+CXM_DEVICE_ATTR tlx::half Subtraction::operator()(const tlx::half Xx, const tlx::half Xy) const noexcept {
+    return Xx - Xy;
+}
+
+CXM_DEVICE_ATTR float Multiplication::operator()(const float Xx, const float Xy) const noexcept {
+    return Xx * Xy;
+}
+
+CXM_DEVICE_ATTR tlx::bfloat16 Multiplication::operator()(const tlx::bfloat16 Xx, const tlx::bfloat16 Xy) const noexcept {
+    return Xx * Xy;
+}
+
+CXM_DEVICE_ATTR tlx::half Multiplication::operator()(const tlx::half Xx, const tlx::half Xy) const noexcept {
+    return Xx * Xy;
+}
+
+CXM_DEVICE_ATTR float Square::operator()(const float Xx) const noexcept {
+    return Xx * Xx;
+}
+
+CXM_DEVICE_ATTR tlx::bfloat16 Square::operator()(const tlx::bfloat16 Xx) const noexcept {
+    return Xx * Xx;
+}
+
+CXM_DEVICE_ATTR tlx::half Square::operator()(const tlx::half Xx) const noexcept {
+    return Xx * Xx;
+}
+
+CXM_DEVICE_ATTR float Power::operator()(const float Xx, const float Xy) const noexcept {
+    return powf(Xx, Xy);
+}
+
+CXM_DEVICE_ATTR tlx::bfloat16 Power::operator()(const tlx::bfloat16 Xx, const tlx::bfloat16 Xy) const noexcept {
+    return powf(Xx, Xy);
+}
+
+CXM_DEVICE_ATTR tlx::half Power::operator()(const tlx::half Xx, const tlx::half Xy) const noexcept {
+    return powf(Xx, Xy);
+}
+
+CXM_DEVICE_ATTR float Sqrt::operator()(const float Xx) const noexcept {
+    return sqrtf(Xx);
+}
+
+CXM_DEVICE_ATTR tlx::bfloat16 Sqrt::operator()(const tlx::bfloat16 Xx) const noexcept {
+    return sqrtf(Xx);
+}
+
+CXM_DEVICE_ATTR tlx::half Sqrt::operator()(const tlx::half Xx) const noexcept {
+    return sqrtf(Xx);
+}
+
+CXM_DEVICE_ATTR float RSqrt::operator()(const float Xx) const noexcept {
+    return rsqrtf(Xx);
+}
+
+CXM_DEVICE_ATTR tlx::bfloat16 RSqrt::operator()(const tlx::bfloat16 Xx) const noexcept {
+    return rsqrtf(Xx);
+}
+
+CXM_DEVICE_ATTR tlx::half RSqrt::operator()(const tlx::half Xx) const noexcept {
+    return rsqrtf(Xx);
+}
+
+CXM_DEVICE_ATTR float Log::operator()(const float Xx) const noexcept {
+    return logf(Xx);
+}
+
+CXM_DEVICE_ATTR tlx::bfloat16 Log::operator()(const tlx::bfloat16 Xx) const noexcept {
+    return logf(Xx);
+}
+
+CXM_DEVICE_ATTR tlx::half Log::operator()(const tlx::half Xx) const noexcept {
+    return logf(Xx);
+}
+
+CXM_DEVICE_ATTR float Exp::operator()(const float Xx) const noexcept {
+    return expf(Xx);
+}
+
+CXM_DEVICE_ATTR tlx::bfloat16 Exp::operator()(const tlx::bfloat16 Xx) const noexcept {
+    return expf(Xx);
+}
+
+CXM_DEVICE_ATTR tlx::half Exp::operator()(const tlx::half Xx) const noexcept {
+    return expf(Xx);
+}
+
+#ifdef __CUDACC_RELAXED_CONSTEXPR__
+#pragma message("TLX: CUDA relaxed constexpr ENABLED")
+#else
+#pragma message("TLX: CUDA relaxed constexpr DISABLED")
+#endif
--- source/CortexMind/framework/Engine/Operations/kernel_ops.cuh
+++ source/CortexMind/framework/Engine/Operations/kernel_ops.cuh
@@ -6,16 +6,98 @@
 #define CORTEXMIND_FRAMEWORK_ENGINE_OPERATIONS_KERNEL_OPS_CUH
 
 #include <CortexMind/framework/Engine/Operations/base.cuh>
+#include <CortexMind/runtime/macros.hpp>
 #include <tlx/types.hpp>
 
 namespace cortex::_fw::ops {
     struct Addition : KernelBase {
         [[nodiscard]]
-        float operator()(float Xx, float Xy) const noexcept;
+        CXM_DEVICE_ATTR float operator()(float Xx, float Xy) const noexcept;
         [[nodiscard]]
-        tlx::bfloat16 operator()(tlx::bfloat16 Xx, tlx::bfloat16 Xy) const noexcept;
+        CXM_DEVICE_ATTR tlx::bfloat16 operator()(tlx::bfloat16 Xx, tlx::bfloat16 Xy) const noexcept;
         [[nodiscard]]
-        tlx::half operator()(tlx::half Xx, tlx::half Xy) const noexcept;
+        CXM_DEVICE_ATTR tlx::half operator()(tlx::half Xx, tlx::half Xy) const noexcept;
+    };
+
+    struct Subtraction : KernelBase {
+        [[nodiscard]]
+        CXM_DEVICE_ATTR float operator()(float Xx, float Xy) const noexcept;
+        [[nodiscard]]
+        CXM_DEVICE_ATTR tlx::bfloat16 operator()(tlx::bfloat16 Xx, tlx::bfloat16 Xy) const noexcept;
+        [[nodiscard]]
+        CXM_DEVICE_ATTR tlx::half operator()(tlx::half Xx, tlx::half Xy) const noexcept;
+    };
+
+    struct Multiplication : KernelBase {
+        [[nodiscard]]
+        CXM_DEVICE_ATTR float operator()(float Xx, float Xy) const noexcept;
+        [[nodiscard]]
+        CXM_DEVICE_ATTR tlx::bfloat16 operator()(tlx::bfloat16 Xx, tlx::bfloat16 Xy) const noexcept;
+        [[nodiscard]]
+        CXM_DEVICE_ATTR tlx::half operator()(tlx::half Xx, tlx::half Xy) const noexcept;
+    };
+
+    struct Division : KernelBase {
+        [[nodiscard]]
+        CXM_DEVICE_ATTR float operator()(float Xx, float Xy) const noexcept;
+        [[nodiscard]]
+        CXM_DEVICE_ATTR tlx::bfloat16 operator()(tlx::bfloat16 Xx, tlx::bfloat16 Xy) const noexcept;
+        [[nodiscard]]
+        CXM_DEVICE_ATTR tlx::half operator()(tlx::half Xx, tlx::half Xy) const noexcept;
+    };
+
+    struct  Square : KernelBase {
+        [[nodiscard]]
+        CXM_DEVICE_ATTR float operator()(float Xx) const noexcept;
+        [[nodiscard]]
+        CXM_DEVICE_ATTR tlx::bfloat16 operator()(tlx::bfloat16 Xx) const noexcept;
+        [[nodiscard]]
+        CXM_DEVICE_ATTR tlx::half operator()(tlx::half Xx) const noexcept;
+    };
+
+    struct Power : KernelBase {
+        [[nodiscard]]
+        CXM_DEVICE_ATTR float operator()(float Xx, float Xy) const noexcept;
+        [[nodiscard]]
+        CXM_DEVICE_ATTR tlx::bfloat16 operator()(tlx::bfloat16 Xx, tlx::bfloat16 Xy) const noexcept;
+        [[nodiscard]]
+        CXM_DEVICE_ATTR tlx::half operator()(tlx::half Xx, tlx::half Xy) const noexcept;
+    };
+
+    struct Sqrt : KernelBase {
+        [[nodiscard]]
+        CXM_DEVICE_ATTR float operator()(float Xx) const noexcept;
+        [[nodiscard]]
+        CXM_DEVICE_ATTR tlx::bfloat16 operator()(tlx::bfloat16 Xx) const noexcept;
+        [[nodiscard]]
+        CXM_DEVICE_ATTR tlx::half operator()(tlx::half Xx) const noexcept;
+    };
+
+    struct RSqrt : KernelBase {
+        [[nodiscard]]
+        CXM_DEVICE_ATTR float operator()(float Xx) const noexcept;
+        [[nodiscard]]
+        CXM_DEVICE_ATTR tlx::bfloat16 operator()(tlx::bfloat16 Xx) const noexcept;
+        [[nodiscard]]
+        CXM_DEVICE_ATTR tlx::half operator()(tlx::half Xx) const noexcept;
+    };
+
+    struct Log : KernelBase {
+        [[nodiscard]]
+        CXM_DEVICE_ATTR float operator()(float Xx) const noexcept;
+        [[nodiscard]]
+        CXM_DEVICE_ATTR tlx::bfloat16 operator()(tlx::bfloat16 Xx) const noexcept;
+        [[nodiscard]]
+        CXM_DEVICE_ATTR tlx::half operator()(tlx::half Xx) const noexcept;
+    };
+
+    struct Exp : KernelBase {
+        [[nodiscard]]
+        CXM_DEVICE_ATTR float operator()(float Xx) const noexcept;
+        [[nodiscard]]
+        CXM_DEVICE_ATTR tlx::bfloat16 operator()(tlx::bfloat16 Xx) const noexcept;
+        [[nodiscard]]
+        CXM_DEVICE_ATTR tlx::half operator()(tlx::half Xx) const noexcept;
     };
 } //namespace cortex::_fw::ops
 
