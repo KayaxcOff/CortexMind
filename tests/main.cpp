b4db297261dc3fb9e77bded8acd9917a2ab9f0df
Muhammet Kaya <muham123cak@gmail.com>
2026-08-15 11:52:46 +0300

    feat: there is a compile flag problem. I will fix it

--- tests/main.cpp
+++ tests/main.cpp
@@ -2,15 +2,25 @@
 // Created by muham on 3.08.2026.
 //
 
-#include "CortexMind/framework/Type/type.hpp"
+#include <CortexMind/framework/Shape/shape.hpp>
 #include <iostream>
 
 using namespace cortex::_fw;
 
 int main() {
-    const TensorType type(DType::BFloat16);
+    TensorShape shape({2, 2});
 
-    std::cout << type.ToString() << std::endl;
+    std::cout << "Shape: {";
+    for (const auto& item : shape.shape()) {
+        std::cout << item;
+    }
+    std::cout << "}" << std::endl;
+
+    std::cout << "Stride: {";
+    for (const auto& item : shape.stride()) {
+        std::cout << item;
+    }
+    std::cout << "}" << std::endl;
 
     return 0;
 }
