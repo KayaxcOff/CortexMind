d0aa52801da96e6121ae70b345d474bf81822183
Muhammet Kaya <muham123cak@gmail.com>
2026-08-17 21:46:08 +0300

    feat: completed matmul

--- source/CortexMind/framework/Engine/CUDA/matrix.cu
+++ source/CortexMind/framework/Engine/CUDA/matrix.cu
@@ -4,8 +4,10 @@
 
 #include "CortexMind/framework/Engine/CUDA/matrix.cuh"
 #include <CortexMind/framework/Engine/CUDA/cast.cuh>
+#include <CortexMind/framework/Engine/CUDA/handle.cuh>
 #include <CortexMind/framework/Engine/Kernels/matrix.cuh>
 #include <CortexMind/framework/Engine/Operations/kernel_ops.cuh>
+#include <CortexMind/framework/Tools/Error/errors.hpp>
 #include <CortexMind/framework/Tools/grid.cuh>
 
 namespace cortex::_fw::nv {
@@ -205,5 +207,137 @@ namespace cortex::_fw::nv {
         const auto Xz4 = convert(Xz);
 
         kernels::BinaryKernel<ops::Min><<<grid_size, kBlockSize>>>(Xx4, Xy4, Xz4, N);
+    }
+
+    template<>
+    void Matrix::matmul<float>(const float* __restrict Xx, const float* __restrict Xy, float* __restrict Xz, const std::size_t xN, const std::size_t yN, const std::size_t zN) {
+        const int M = static_cast<int>(xN);
+        const int K = static_cast<int>(yN);
+        const int N = static_cast<int>(zN);
+
+        const handle h;
+
+        constexpr float alpha = 1.0f;
+        constexpr float beta  = 0.0f;
+
+        const cublasStatus_t status = cublasGemmEx(
+            h,
+
+            CUBLAS_OP_N,
+            CUBLAS_OP_N,
+
+            N,
+            M,
+            K,
+
+            &alpha,
+
+            Xy,
+            CUDA_R_32F,
+            N,
+
+            Xx,
+            CUDA_R_32F,
+            K,
+
+            &beta,
+
+            Xz,
+            CUDA_R_32F,
+            N,
+
+            CUBLAS_COMPUTE_32F_FAST_TF32,
+            CUBLAS_GEMM_DEFAULT_TENSOR_OP
+        );
+
+        CXM_ASSERT(status != CUBLAS_STATUS_SUCCESS, "matmul on CUDA has failed");
+    }
+
+    template<>
+    void Matrix::matmul<tlx::bfloat16>(const tlx::bfloat16* __restrict Xx, const tlx::bfloat16* __restrict Xy, tlx::bfloat16* __restrict Xz, const std::size_t xN, const std::size_t yN, const std::size_t zN) {
+        const int M = static_cast<int>(xN);
+        const int K = static_cast<int>(yN);
+        const int N = static_cast<int>(zN);
+
+        const handle h;
+
+        constexpr float alpha = 1.0f;
+        constexpr float beta  = 0.0f;
+
+        const cublasStatus_t status = cublasGemmEx(
+            h,
+
+            CUBLAS_OP_N,
+            CUBLAS_OP_N,
+
+            N,
+            M,
+            K,
+
+            &alpha,
+
+            Xy,
+            CUDA_R_16BF,
+            N,
+
+            Xx,
+            CUDA_R_16BF,
+            K,
+
+            &beta,
+
+            Xz,
+            CUDA_R_16BF,
+            N,
+
+            CUBLAS_COMPUTE_32F,
+            CUBLAS_GEMM_DEFAULT_TENSOR_OP
+        );
+
+        CXM_ASSERT(status != CUBLAS_STATUS_SUCCESS, "matmul on CUDA has failed");
+    }
+
+    template<>
+    void Matrix::matmul<tlx::half>(const tlx::half* __restrict Xx, const tlx::half* __restrict Xy, tlx::half* __restrict Xz, const std::size_t xN, const std::size_t yN, const std::size_t zN) {
+        const int M = static_cast<int>(xN);
+        const int K = static_cast<int>(yN);
+        const int N = static_cast<int>(zN);
+
+        const handle h;
+
+        const tlx::half alpha = 1.0f;
+        const tlx::half beta  = 0.0f;
+
+        const cublasStatus_t status = cublasGemmEx(
+            h,
+
+            CUBLAS_OP_N,
+            CUBLAS_OP_N,
+
+            N,
+            M,
+            K,
+
+            &alpha,
+
+            Xy,
+            CUDA_R_16F,
+            N,
+
+            Xx,
+            CUDA_R_16F,
+            K,
+
+            &beta,
+
+            Xz,
+            CUDA_R_16F,
+            N,
+
+            CUBLAS_COMPUTE_16F,
+            CUBLAS_GEMM_DEFAULT_TENSOR_OP
+        );
+
+        CXM_ASSERT(status != CUBLAS_STATUS_SUCCESS, "matmul on CUDA has failed");
     }
 } //namespace cortex::_fw::nv
