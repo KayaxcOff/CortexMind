ef5c571d3408ad1e24bccc87717b12f3d8d20a56
Muhammet Kaya <muham123cak@gmail.com>
2026-08-17 13:10:08 +0300

    feat: remove `move()` function

--- source/CortexMind/framework/Graph/link.cpp
+++ source/CortexMind/framework/Graph/link.cpp
@@ -4,13 +4,12 @@
 
 #include "CortexMind/framework/Graph/link.hpp"
 #include <CortexMind/framework/Tensor/tensor.hpp>
-#include <tlx/utility.hpp>
 
 using namespace cortex::_fw::meta;
 using namespace cortex::_fw;
 
 GradientLink::GradientLink(Tensor &t, const std::shared_ptr<GradientFlow> &flow) {
-    t.flow_ = tlx::move(flow);
+    t.flow_ = flow;
 }
 
 GradientLink::~GradientLink() = default;
--- source/CortexMind/framework/Graph/link.hpp
+++ source/CortexMind/framework/Graph/link.hpp
@@ -9,8 +9,28 @@
 #include <memory>
 
 namespace cortex::_fw::meta {
+    /**
+     * @brief Links a tensor to its gradient flow.
+     *
+     * Associates the specified tensor with a gradient flow responsible for
+     * propagating the derivative of the operation that produced the tensor.
+     *
+     * This is primarily used by activation and other differentiable layers
+     * to attach the derivative operation of an output tensor to the
+     * corresponding gradient computation graph.
+     */
     class GradientLink {
     public:
+        /**
+         * @brief Associates a tensor with a gradient flow.
+         *
+         * Assigns the specified gradient flow to the tensor, establishing the
+         * connection required for gradient propagation during backpropagation.
+         *
+         * @param t Tensor whose gradient flow will be associated.
+         * @param flow Gradient flow representing the derivative operation
+         *             associated with the tensor.
+         */
         GradientLink(Tensor& t, const std::shared_ptr<GradientFlow> &flow);
         GradientLink(const GradientLink&) = delete;
         GradientLink(GradientLink&&) = delete;
