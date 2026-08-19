71e805f13bfeff2cd912cd036b8e964581666180
Muhammet Kaya <muham123cak@gmail.com>
2026-08-19 12:19:03 +0300

    feat: restart to working on cpu-kernels

--- source/CortexMind/framework/Engine/Operations/kernel_ops.hpp
+++ source/CortexMind/framework/Engine/Operations/kernel_ops.hpp
@@ -7,6 +7,7 @@
 
 #include <CortexMind/framework/Engine/Operations/base.hpp>
 #include <CortexMind/framework/Engine/AVX2/functions.hpp>
+#include <cmath>
 
 namespace cortex::_fw::ops {
     struct Add : KernelBase {
@@ -50,6 +51,50 @@ namespace cortex::_fw::ops {
         [[nodiscard]]
         float operator()(const float Xx, const float Xy) const noexcept {
             return Xx / Xy;
+        }
+    };
+
+    struct Square : KernelBase {
+        [[nodiscard]]
+        avx2::vec8f operator()(const avx2::vec8f& Xx) const noexcept {
+            return avx2::square(Xx);
+        }
+        [[nodiscard]]
+        float operator()(const float Xx) const noexcept {
+            return Xx * Xx;
+        }
+    };
+
+    struct Power : KernelBase {
+        [[nodiscard]]
+        avx2::vec8f operator()(const avx2::vec8f& Xx, const avx2::vec8f& Xy) const noexcept {
+            return avx2::pow(Xx, Xy);
+        }
+        [[nodiscard]]
+        float operator()(const float Xx, const float Xy) const noexcept {
+            return std::pow(Xx, Xy);
+        }
+    };
+
+    struct Sqrt : KernelBase {
+        [[nodiscard]]
+        avx2::vec8f operator()(const avx2::vec8f& Xx) const noexcept {
+            return avx2::sqrt(Xx);
+        }
+        [[nodiscard]]
+        float operator()(const float Xx) const noexcept {
+            return std::sqrt(Xx);
+        }
+    };
+
+    struct RSqrt : KernelBase {
+        [[nodiscard]]
+        avx2::vec8f operator()(const avx2::vec8f& Xx) const noexcept {
+            return avx2::rsqrt(Xx);
+        }
+        [[nodiscard]]
+        float operator()(const float Xx) const noexcept {
+            return 1.f / std::sqrt(Xx);
         }
     };
 } //namespace cortex::_fw::ops
